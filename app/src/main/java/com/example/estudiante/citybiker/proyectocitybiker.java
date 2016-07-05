9a7e6fc00ce8cdc584bb23b5a42f910cb310e4f9
SolangeGranja <chikisol-7078@hotmail.com>
2016-07-05 10:11:33 -0500

    Commit nuevo

---
+++ app/src/main/java/com/example/estudiante/citybiker/proyectocitybiker.java
@@ -0,0 +1,26 @@
+package com.example.estudiante.citybiker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class proyectocitybiker extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proyecto_city_biker);
+    }
+public void cargar (View v) {
+    Intent i = new Intent(this, pantalla2.class);
+    startActivity(i);
+}
+     public void jugar (View v){
+        Intent i = new Intent(this, pantalla3.class);
+         startActivity(i);
+
+    }
+
+}
