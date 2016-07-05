0c1a6e4450c3cba6cace326a1f776cb3e1bb2c7f
SolangeGranja <monitochavez26@hotmail.com>
2016-07-05 09:43:34 -0500

    Commit nuevo

---
+++ app/src/main/java/com/example/estudiante/citybiker/pantalla3.java
@@ -0,0 +1,13 @@
+package com.example.estudiante.citybiker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pantalla3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla3);
+    }
+}
